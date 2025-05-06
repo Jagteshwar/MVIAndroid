f88a291a6e268779cfa77d3d8322efeacea9f18c
Jagteshwar <jagteshwars3@gmail.com>
2025-05-06 19:12:53 -0400

    WIP

--- app/src/main/java/com/jagteshwar/mviandroid/GetPost.kt
+++ app/src/main/java/com/jagteshwar/mviandroid/GetPost.kt
@@ -3,6 +3,7 @@ package com.jagteshwar.mviandroid
 import com.jagteshwar.mviandroid.network.PostApi
 import com.jagteshwar.mviandroid.network.model.Post
 import com.jagteshwar.mviandroida.DataState
+import com.jagteshwar.mviandroida.UiComponent
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
@@ -12,7 +13,16 @@ class GetPost(
 
     fun execute(): Flow<DataState<List<Post>>> {
         return flow {
-
+            emit(DataState.Loading(true))
+            try {
+                val posts = postApi.getPosts()
+                emit(DataState.Success(data = posts))
+            } catch (e: Exception) {
+                e.printStackTrace()
+                emit(DataState.Error(UiComponent.Toast(e.message ?: "Unknown error.")))
+            } finally {
+                emit(DataState.Loading(false))
+            }
         }
     }
 }
